3caad4d1f62d9fc76c6235e0d6a485a9cdcaa474
Naveen Basireddy <naveenkumar.basireddy@gmail.com>
2021-09-14 22:54:10 +0530

    Added Output Field

--- script.js
+++ script.js
@@ -9,9 +9,31 @@ function fileClick() {
   document.getElementById("file").className = "card__file-on";
 }
 
+function submitClick() {
+  document.getElementById("output").className = "output__on"
+}
+
+function copyText() {
+  debugger;
+  var copyValue = document.getElementById("outputString");
+
+  document.getElementById("outputString").className = "output__string-on";
+
+  var copyString = copyValue.value
+  // copyValue.select();
+  // copyString.select();
+  copyValue.focus();
+  // copyValue.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyString);
+  console.log(copyString);
+  // document.execCommand("copy");
+}
+
 var value = document.getElementById("value").value;
 var stringKey = document.getElementById("key").value;
 var file = document.getElementById("File").value;
+var stringAlgo = document.getElementsByName("stringAlgo").value;
+var stringMode = document.getElementsByName("stringMode").value;
 
 
 

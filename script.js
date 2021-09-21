d941db9443e1a445557b1458f28ddad2106a161d
Naveen Basireddy <naveenkumar.basireddy@gmail.com>
2021-09-21 12:19:11 +0530

    Added Dropmenu

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 `use strict`;
+
+
+
+
+
 function stringClick() {
   document.getElementById("string").className = "card__string-on";
   document.getElementById("file").className = "card__file";
@@ -9,33 +14,16 @@ function fileClick() {
   document.getElementById("file").className = "card__file-on";
 }
 
-function submitClick() {
-  document.getElementById("output").className = "output__on"
-}
 
 function copyText() {
-  debugger;
   var copyValue = document.getElementById("outputString");
-
-  document.getElementById("outputString").className = "output__string-on";
-
-  var copyString = copyValue.value
-  // copyValue.select();
-  // copyString.select();
-  copyValue.focus();
-  // copyValue.setSelectionRange(0, 99999);
+  var copyString = copyValue.innerHTML.trim();
   navigator.clipboard.writeText(copyString);
-  console.log(copyString);
-  // document.execCommand("copy");
+  document.getElementById("outputString").className = "output__string-on";
+  setTimeout(remove, 800);
 }
 
-var value = document.getElementById("value").value;
-var stringKey = document.getElementById("key").value;
-var file = document.getElementById("File").value;
-var stringAlgo = document.getElementsByName("stringAlgo").value;
-var stringMode = document.getElementsByName("stringMode").value;
-
-
-
-
+function remove() {
+  document.getElementById("outputString").className = "output__string";
+}
 
